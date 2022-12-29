2a5ca559bea4a1d5a39f50bbdc671adae045f5a2
leojazz28 <leonardosousa.linux@gmail.com>
2022-12-29 18:57:34 -0300

    commit 01

---
+++ Estudos/src/Figura.java
@@ -0,0 +1,20 @@
+public class Figura {
+    private String cor;
+
+    public Figura() {}
+
+    public Figura(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor( String cor) {
+        this.cor = cor;
+    }
+
+
+
+}
---
+++ Estudos/src/Retangulo.java
@@ -0,0 +1,34 @@
+public class Retangulo {
+    private Double lado1;
+    private Double lado2;
+
+    public Retangulo() {}
+
+    public Retangulo(Double lado1, Double lado2, String cor) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    public Double getLado1() {
+        return lado1;
+    }
+
+    public Double getLado2() {
+        return lado2;
+    }
+
+    public void setLado1(Double lado1) {
+        this.lado1 = lado1;
+    }
+
+    public void setLado2(Double lado2) {
+        this.lado2 = lado2;
+    }
+
+    public Double area() {
+
+    }
+
+
+}
+
